eb829987800c4b87401ce07db4ee85a83d2a35ce
Swagat003 <109663081+Swagat003@users.noreply.github.com>
2023-12-27 19:45:51 +0530

    loader

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-let them = document.querySelector('#them-icon')
+let them = document.querySelector('#them-icon');
 
 them.addEventListener('click', (evt) => {
-    let them_icon = document.querySelector('#them-icon')
-    let body = document.querySelector('body')
-    let content = document.querySelector('#content')
-    let skills = document.getElementsByClassName('code-lang')
-    body.classList.toggle('dark-body')
-    content.classList.toggle('dark-content')
-    them_icon.classList.toggle('dark-them-icon')
+    let them_icon = document.querySelector('#them-icon');
+    let body = document.querySelector('body');
+    let content = document.querySelector('#content');
+    let skills = document.getElementsByClassName('code-lang');
+    body.classList.toggle('dark-body');
+    content.classList.toggle('dark-content');
+    them_icon.classList.toggle('dark-them-icon');
 
     for (skill of skills) {
-        skill.classList.toggle('dark-code-lang')
+        skill.classList.toggle('dark-code-lang');
     }
 
+})
+
+let loader = document.getElementById('loader');
+window.addEventListener('load',(e)=>{
+    loader.style.display='none'
 })
